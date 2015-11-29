ae8e0fbd0374ada8dcf82c0ce7d3809debb3591c
John Shrader <figurefactory@gmail.com>
2015-11-29 00:43:16 -0500

    fixed extra comma

--- fight_picks.js
+++ fight_picks.js
@@ -75,7 +75,7 @@ Template.pickArea.events({
        finish:$('input[name="finish3"]:checked').val(), 
        round:$('input[name="rd3"]:checked').val()
       },
-      {, 
+      { 
        fightNumber:4,
        fighter:$('input[name="winner4"]:checked').val(), 
        finish:$('input[name="finish4"]:checked').val(), 
